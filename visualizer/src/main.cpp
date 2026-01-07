663afb6166d287761e273cf6d1cd695b21b4c74a
hyperion64 <m.barnstorf@tutanota.com>
2026-01-07 10:39:09 +0100

    add basic visualizer cli

--- visualizer/src/main.cpp
+++ visualizer/src/main.cpp
@@ -1,13 +1,22 @@
 #include <curl/curl.h>
 
+#include <charconv>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <string>
 
 #include "model.hpp"
 #include "parser.hpp"
 #include "svg_graph.hpp"
+
+bool is_number(const std::string& s) {
+    for (unsigned char c : s) {
+        if (!std::isdigit(c)) return false;
+    }
+    return true;
+}
 
 static size_t append_cb(char* ptr, size_t size, size_t nmemb, void* userdata) {
     auto* s = static_cast<std::string*>(userdata);
@@ -31,14 +40,22 @@ std::string http_post(const std::string& url, const std::string& data) {
     return out;
 }
 
-int main() {
-    uint64_t job_id = 1;
-    std::string cluster_name = "cname1";
-    std::string payload = R"({"job_id":)" + std::to_string(job_id)
-                          + R"(,"cluster_name":")" + cluster_name + R"("})";
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <cluster name> <job id>\n";
+        return EXIT_FAILURE;
+    }
+    std::string cluster_name = argv[1];
+    std::string job_id = argv[2];
+    if (!is_number(job_id)) {
+        std::cerr << "Error: job_id must be a positive number, got: " << argv[2]
+                  << "\n";
+        return 1;
+    }
+    std::string payload = R"({"job_id":)" + job_id + R"(,"cluster_name":")"
+                          + cluster_name + R"("})";
     std::string url = "http://127.0.0.1:9000/graph_api";
     std::string body = http_post(url, payload);
-    // std::cout << "Parsed data: " << body << std::endl;
     ParsedLibcprovData parsed_libcprov_data = parse_injector_data(body);
     if (parsed_libcprov_data.response_type == ResponseType::ProvData) {
         build_graph(parsed_libcprov_data.job_data.value());
